ed060f205f39b29466d89addf4fa6dd5d8f3c596
Zion Henry <101232420@georgebrown.ca>
2021-09-29 16:16:46 -0400

    Initial commit

---
+++ PetClinic/src/main/java/ca/gbc/comp3095/comp3095petclinic/Comp3095PetClinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.comp3095petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3095PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp3095PetClinicApplication.class, args);
+    }
+
+}
